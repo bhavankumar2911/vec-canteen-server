6484cc29f0c0e3d763f57f5969266052170dee03
Bhavan Kumar <bhavankumarcse2020@gmail.com>
2022-03-26 20:40:39 +0530

    fetch all users

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 const create = require("../controllers/user/create");
 const readOne = require("../controllers/user/readOne");
 const login = require("../controllers/user/login");
+const readAll = require("../controllers/user/readAll");
 
 router.post("/", create);
 
 router.get("/:username", readOne);
+
+router.get("/", readAll);
 
 router.post("/login", login);
 
